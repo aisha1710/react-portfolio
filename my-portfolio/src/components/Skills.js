f0f79992844b907f6c9959df5e95d049b56ecc54
aisha <faisha879@gmail.com>
2022-12-07 21:05:32 +0000

    committing about me section

---
+++ my-portfolio/src/components/Skills.js
@@ -0,0 +1,39 @@
+import "react-multi-carousel/lib/styles.css";
+import { Container, Row, Col } from "react-bootstrap";
+import image from "../assets/img/image.jpg";
+
+export const Skills = () => {
+  return (
+    <section className="about-me" id="skills">
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="box wow zoomIn">
+              <h2>About me</h2>
+              <Container>
+                <Row className="align-items-center">
+                  <Col xs={12} md={6} xl={7}>
+                    <p>
+                      An aspiring <b>full-stack developer</b> who enjoys
+                      problem-solving and creating user interfaces with vital
+                      customer service experience. Currently, studying a
+                      Full-Stack Coding Boot camp at the{" "}
+                      <b> University of Birmingham</b>. My hobbies include
+                      photography,reading and basketball. I enjoy traveling and
+                      trying new things.
+                    </p>
+                  </Col>
+                  <Col xs={12} md={6} xl={5}>
+                    <div>
+                      <img src={image} alt="My image" />
+                    </div>
+                  </Col>
+                </Row>
+              </Container>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
